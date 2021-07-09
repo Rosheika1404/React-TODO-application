d3b9fad692d11e56f276320efa95e0ef368db668
Sheika1404 <rosheika1404@gmail.com>
2021-07-09 20:43:01 +0000

    redo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,43 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [list, setList] = useState([]);
+	const [todo, setTodo] = useState("");
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 notepad">
+			<div className="list">
+				<div className="bar"></div>
+				<h1>To-Do List</h1>
+				<input
+					className="input"
+					value={todo}
+					placeholder="What do you need to do?"
+					onChange={e => setTodo(e.target.value)}
+				/>
+
+				<button
+					onClick={() => {
+						setList([...list, todo]);
+						setTodo("");
+					}}
+					className="btn btn-success m-2">
+					Add
+				</button>
+				{list.map((element, index) => (
+					<div key={index}>
+						{element}
+						<button
+							onClick={() =>
+								setList(
+									list.filter(remove => remove !== element)
+								)
+							}
+							className="btn btn-warning"></button>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
